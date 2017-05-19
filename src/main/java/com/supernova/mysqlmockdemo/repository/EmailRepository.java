244079b58a5c161eb393b20913c8bea3caa195cd
Yougu Yuan <yougu.yuan@supernovacompanies.com>
2017-05-19 09:42:36 -0500

    Now use CrudRepository and entity.

--- src/main/java/com/supernova/mysqlmockdemo/repository/EmailRepository.java
+++ src/main/java/com/supernova/mysqlmockdemo/repository/EmailRepository.java
@@ -1,22 +1,10 @@
 package com.supernova.mysqlmockdemo.repository;
 
-import org.slf4j.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class EmailRepository {	
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
+import com.supernova.mysqlmockdemo.models.UserEmail;
 
-	public int count() {
-		if (jdbcTemplate == null) {
-			logger.error("jdbcTemplate autowire failed...");
-		}
-		final String CNT_QUERY = "select count(*) from tiger_db.t_emails";
-		Number count = jdbcTemplate.queryForObject(CNT_QUERY, Integer.class);
-		return (count != null ? count.intValue() : 0);
-	}	
+@Repository
+public interface EmailRepository extends CrudRepository<UserEmail, Integer>  {	
 }
