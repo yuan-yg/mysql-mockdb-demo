91e18bf687dd67f4020ce933afe14c30fe9781d0
Yougu Yuan <yougu.yuan@supernovacompanies.com>
2017-05-19 09:23:20 -0500

    comment out unused properties.

--- src/test/java/com/supernova/mysqlmockdemo/embedded/EmbeddedMysqlManager.java
+++ src/test/java/com/supernova/mysqlmockdemo/embedded/EmbeddedMysqlManager.java
@@ -3,14 +3,9 @@ package com.supernova.mysqlmockdemo.embedded;
 import org.slf4j.*;
 
 import org.springframework.beans.factory.DisposableBean;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestComponent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
-
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import com.wix.mysql.EmbeddedMysql;
 import com.wix.mysql.config.MysqldConfig;
@@ -30,7 +25,6 @@ public class EmbeddedMysqlManager implements DisposableBean {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	private JdbcTemplate jdbcTemplate;
 	private MysqlDataSource dataSource;
 
 	private EmbeddedMysql mysqld;
@@ -39,9 +33,10 @@ public class EmbeddedMysqlManager implements DisposableBean {
 	// use "root"is NOT allowed.
 	private final String DB_USER = "testRoot";
 	private final String DB_PASSWD = "testPwd";
-	private final int DB_PORT = 4408;
+	
+	private int dbPort = 4408;
 
-	private final String DB_SCHEMA_NAME = "tiger_db";
+	private String dbSchemaName="tiger_db";
 
 	@Bean
 	public DataSource getDataSource() {
@@ -50,31 +45,22 @@ public class EmbeddedMysqlManager implements DisposableBean {
 			startDatabase();
 			
 			dataSource = new MysqlDataSource();
-			dataSource.setUrl("jdbc:mysql://localhost:" + DB_PORT + "/" + DB_SCHEMA_NAME);
+			dataSource.setUrl("jdbc:mysql://localhost:" + dbPort + "/" + dbSchemaName);
 			dataSource.setUser(DB_USER);
 			dataSource.setPassword(DB_PASSWD);
 		}
 		return dataSource;
 	}
-
-//	@Bean
-//	public JdbcTemplate getJdbcTemplate() {
-//		if (jdbcTemplate == null) {
-//			logger.info("getJdbcTemplate create bean...");
-//			jdbcTemplate = new JdbcTemplate(getDataSource());
-//		}
-//		return jdbcTemplate;
-//	}
 	
 	public void reloadSchema() {
 		mysqld.reloadSchema(schemaConfig);
 	}
 
 	private void startDatabase() {
-		MysqldConfig dbConfig = aMysqldConfig(v5_7_17).withCharset(UTF8).withPort(DB_PORT).withUser(DB_USER, DB_PASSWD)
+		MysqldConfig dbConfig = aMysqldConfig(v5_7_17).withCharset(UTF8).withPort(dbPort).withUser(DB_USER, DB_PASSWD)
 				.build();
 
-		schemaConfig = aSchemaConfig(DB_SCHEMA_NAME)
+		schemaConfig = aSchemaConfig(dbSchemaName)
 				.withScripts(classPathScript("sql/mysql/test-only/tiger_db-20170517.sql")).withCharset(UTF8).build();
 
 		logger.info("embedded mysql starting ...");
